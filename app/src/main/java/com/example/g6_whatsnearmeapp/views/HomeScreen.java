345799a5fb983043732d4b8b6f5ae247aa7a7825
Harshitha Sompura <sompharshi123@gmail.com>
2022-06-12 12:29:11 -0400

    Set up autocomplete Places SDK

--- app/src/main/java/com/example/g6_whatsnearmeapp/views/HomeScreen.java
+++ app/src/main/java/com/example/g6_whatsnearmeapp/views/HomeScreen.java
@@ -7,13 +7,22 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.util.Log;
 import android.view.MenuItem;
 import android.os.Bundle;
 
 import com.example.g6_whatsnearmeapp.R;
 import com.example.g6_whatsnearmeapp.databinding.ActivityHomeScreenBinding;
 import com.example.g6_whatsnearmeapp.databinding.ActivityLoginBinding;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Arrays;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -41,6 +50,36 @@ public class HomeScreen extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
         BottomNavigationView bottomNav = binding.bottomNavView;
         NavigationUI.setupWithNavController(bottomNav, navController);
+
+        //initializing places
+        String apiString = getResources().getString(R.string.google_places_api_key);
+        Places.initialize(getApplicationContext(), apiString);
+
+        //places client instance
+        PlacesClient placesClient = Places.createClient(this);
+
+        //autocomplete
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.ADDRESS,
+                Place.Field.LAT_LNG, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                //places.getName will display the name we select
+                Log.i("Places", "Place: " + place.getName() + ", " + place.getId());
+            }
+
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.i("Places", "An error occurred: " + status);
+            }
+        });
 
     }
     // logout handler
