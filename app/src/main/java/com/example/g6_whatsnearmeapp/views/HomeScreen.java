c42669fd8eca6b5fa4df180adadb362542f1bd28
Harshitha Sompura <sompharshi123@gmail.com>
2022-06-12 17:44:25 -0400

    Completed getting user's current location

--- app/src/main/java/com/example/g6_whatsnearmeapp/views/HomeScreen.java
+++ app/src/main/java/com/example/g6_whatsnearmeapp/views/HomeScreen.java
@@ -3,13 +3,25 @@ package com.example.g6_whatsnearmeapp.views;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.util.Log;
 import android.view.MenuItem;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.g6_whatsnearmeapp.R;
 import com.example.g6_whatsnearmeapp.databinding.ActivityHomeScreenBinding;
@@ -23,10 +35,13 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements LocationListener {
 
     private ActivityHomeScreenBinding binding;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +96,23 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
 
+        //Get location permisions from the user
+        if (ContextCompat.checkSelfPermission(HomeScreen.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(HomeScreen.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },100);
+        }
+
+        //get current location button handler
+        binding.btnGetCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CurrentLocation:", "Button Clicked!");
+                getLocation();
+            }
+        });
+
     }
     // logout handler
     @Override
@@ -91,5 +123,50 @@ public class HomeScreen extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //get current location config
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5,HomeScreen.this);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        try {
+            Geocoder geocoder = new Geocoder(HomeScreen.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            String address = addresses.get(0).getAddressLine(0);
+
+            Log.d("CurrentLocation:", address);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
